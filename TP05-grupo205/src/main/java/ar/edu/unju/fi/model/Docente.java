0c2324a02b5be04610080db6d064414759ce62b8
PC <PC@DESKTOP-75A9JNB>
2024-06-13 18:18:36 -0300

    Inicializacion model Docente - Cayo Juan

---
+++ TP05-grupo205/src/main/java/ar/edu/unju/fi/model/Docente.java
@@ -0,0 +1,11 @@
+package ar.edu.unju.fi.model;
+
+import lombok.Data;
+@Data
+public class Docente {
+	private String legajo;
+	private String nombre;
+	private String apellido;
+	private String email;
+	private String telefono;
+}
